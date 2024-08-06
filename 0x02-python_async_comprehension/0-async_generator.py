fb22d279bdaf07287304f1e30872d681ec99f1c9
Leornadia <leornadiajose@gmail.com>
2024-08-06 12:31:00 -0700

    0-async_generator.py

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -8,14 +8,8 @@ async def async_generator() -> AsyncGenerator[float, None]:
     """
     Asynchronously yields 10 random numbers between 0 and 10.
 
-    Each number is generated concurrently after waiting for 1 second.
+    Each number is generated after waiting for 1 second.
     """
-
-    async def generate_random_number():
-        """Helper coroutine to generate a random number after 1 second."""
+    for _ in range(10):
         await asyncio.sleep(1)
-        return random.uniform(0, 10)
-
-    tasks = [asyncio.create_task(generate_random_number()) for _ in range(10)]
-    for task in asyncio.as_completed(tasks):
-        yield await task
+        yield random.uniform(0, 10) 
